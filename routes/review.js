10aea0ae5c60543a9d975f3f56102be97c2457c6
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-02-24 21:25:18 +0200

    Review: Add JWT verification before request

--- routes/review.js
+++ routes/review.js
@@ -6,6 +6,18 @@ var Offert = require('../models/offert');
 
 const ACCEPTED = 'accepted';
 
+router.use('/', function (req, res, next) {
+    jwt.verify(req.query.token, 'secret', function (err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                title: 'Not Authenticated',
+                error: err
+            });
+        }
+        next();
+    })
+});
+
 router.get('/:userId', function (req, res, next) {
     Offert.find({ offererId: req.params.userId, status: ACCEPTED })
         .select('adId reviewId')
