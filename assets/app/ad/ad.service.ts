01608e6665d37734ede2fc66a3140d71896c0a5c
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-02-28 11:57:18 +0200

    AdService: Add methods to get all ads

--- assets/app/ad/ad.service.ts
+++ assets/app/ad/ad.service.ts
@@ -1,37 +1,79 @@
-import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
-import {Observable} from "rxjs/Observable";
-import {AdModel} from "./ad.model";
+import { Injectable } from "@angular/core";
+import { Http, Headers, Response } from "@angular/http";
+import { AdModel } from "./ad.model";
+import { ErrorService } from "../error/error.service";
+
+import 'rxjs/add/operator/map';
+import { Observable } from "rxjs";
+
+export const SERVER_PATH = 'http://localhost:3000/api/anunturi';
 
 @Injectable()
 export class AdService {
-    constructor(private http: Http) {
-    }
+    constructor(private http: Http, private errorService: ErrorService) { }
 
-    saveAd(ad: any) {
+    saveAd(ad: AdModel) {
         const body = JSON.stringify(ad);
-        const headers = new Headers({'Content-Type': 'application/json'});
+        const headers = new Headers({ 'Content-Type': 'application/json' });
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.post('http://localhost:3000/ad' + token, body, {headers: headers})
+        return this.http.post(SERVER_PATH + '/adauga-anunt' + token, body, { headers: headers })
             .map((response: Response) => {
-                const result = response.json();
-                return result.obj._id;
+                return response.json();
             })
-            .catch((error: Response) => {
-                this.errorService.handleError(error.json());
-                return Observable.throw(error.json())
-            });
+            .catch((error: Response) => { return Observable.throw(error.json()) });
+    }
+
+    getAllAds() {
+        return this.http.get(SERVER_PATH)
+            // Map automatically convert response to Observable
+            .map((response: Response) => {
+                const ads = response.json().result;
+                let adsArray: AdModel[] = [];
+
+                for (const ad of ads) {
+                    adsArray.push(new AdModel(ad._id, undefined, ad.title, ad.description, undefined, ad.location, ad.expirationDate));
+                }
+
+                return adsArray;
+            })
+            // Catch don't convert response to Observabla
+            .catch((error: Response) => { return Observable.throw(error.json()) });
+    }
+
+    getUserAds() {
+        const token = localStorage.getItem('token') ? '?token=' + localStorage.getItem('token') : '';
+        return this.http.get(SERVER_PATH + '/my-account/anunturi' + token)
+            .map((response: Response) => {
+                console.log(response);
+                const ads = response.json().result;
+                let adsArray: AdModel[] = [];
+
+                for (const ad of ads) {
+                    adsArray.push(new AdModel(ad._id, undefined, ad.title, undefined, undefined, undefined, ad.expirationDate));
+                }
+
+                return adsArray;
+            })
+            .catch((error: Response) => { return Observable.throw(error) });
     }
 
     getAdsByCategory(categoryName: string) {
-        return this.http.get('http://localhost:3000/' + categoryName)
+        return this.http.get(SERVER_PATH + '/' + categoryName)
             .map((response: Response) => {
-                const ads = response.json.objl
+                const ads = response.json().result;
                 let adsFromCategory: AdModel[] = [];
                 for (let ad of ads) {
-                    let currentAd = new AdModel();
+                    let currentAd = new AdModel(
+                        ad._id,
+                        undefined,
+                        ad.title,
+                        ad.description,
+                        categoryName,
+                        ad.location,
+                        ad.expirationDate
+                    );
                     adsFromCategory.push(currentAd);
                 }
                 return adsFromCategory;
@@ -43,7 +85,7 @@ export class AdService {
     }
 
     getCategories() {
-        return this.http.get('http://localhost:3000')
+        return this.http.get('http://localhost:3000/api/categorii')
             .map((response: Response) => {
                 return response;
             })
