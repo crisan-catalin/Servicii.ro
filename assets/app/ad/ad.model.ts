3037f668ae9d083dd367c76849aee5e2c49bd18c
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-02-28 11:30:06 +0200

    AdModel: Make fields optionals

--- assets/app/ad/ad.model.ts
+++ assets/app/ad/ad.model.ts
@@ -1,3 +1,11 @@
 export class AdModel {
-    constructor(public categoryName: string){}
+    constructor(
+        public id?: string,
+        public userId?: string,
+        public title?: string,
+        public description?: string,
+        public categoryName?: string,
+        public location?: { lat: number, lng: number },
+        public expirationDate?: Date
+    ) { }
 }
