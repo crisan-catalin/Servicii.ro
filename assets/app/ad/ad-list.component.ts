0b90ba0a3f5b1ac0f3e8b8915f81d0ea2d072abc
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-03-01 22:06:49 +0200

    AdList: implement

--- assets/app/ad/ad-list.component.ts
+++ assets/app/ad/ad-list.component.ts
@@ -1,12 +1,22 @@
-import {Component} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AdService } from "./ad.service";
+import { AdModel } from "./ad.model";
 
 @Component({
     selector: 'my-ad-list',
-    styles: [`
-        
-    `],
     templateUrl: './ad-list.component.html'
 })
-export class AdListComponent {
-    ads = ["ad", "ad"];
+export class AdListComponent implements OnInit {
+
+    private ads: AdModel[];
+
+    constructor(private _adService: AdService) { }
+
+    ngOnInit() {
+        this._adService.getAllAds()
+            .subscribe(
+                (data: AdModel[]) => this.ads = data,
+                error => console.error(error)
+            );
+    }
 }
