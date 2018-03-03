c16c36c681238a63e206a6a58986773b6e0b0e9f
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-03-03 11:45:58 +0200

    AdInfo: implement

--- assets/app/ad/ad-info.component.ts
+++ assets/app/ad/ad-info.component.ts
@@ -1,10 +1,87 @@
-import {Component} from "@angular/core";
+import { Component, OnInit, AfterContentInit } from "@angular/core";
+import { AdService } from "./ad.service";
+import { AdModel } from "./ad.model";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthService } from "../auth/auth.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { OffertModel } from "../offerts/offert.model";
+import { OffertService } from "../offerts/offert.service";
 
 @Component({
     selector: "my-ad-info",
     styles: [``],
     templateUrl: './ad-info.component.html'
 })
-export class AdInfoComponent {
+export class AdInfoComponent implements OnInit {
 
+    offertForm: FormGroup;
+
+    ad: AdModel;
+    phone: String;
+    distance: Number;
+    remainingTime: String;
+    isMobile: Boolean;
+
+    constructor(
+        private adService: AdService,
+        private authService: AuthService,
+        private offertService: OffertService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
+
+    ngOnInit() {
+        let adId = this.route.snapshot.params['id'];
+
+        if (adId !== undefined) {
+            this.adService.getAd(adId)
+                .subscribe(
+                    data => {
+                        this.ad = data.result;
+                        this.phone = data.result.userId.phone;
+                        this.remainingTime = this.adService.getRemainingTime(String(this.ad.expirationDate));
+                        this.getDistance();
+                        this.isMobile = this.authService.isMobileDevice();
+                    },
+                    error => this.router.navigateByUrl('/')
+                )
+
+            this.offertForm = new FormGroup({
+                solution: new FormControl(null, Validators.required),
+                price: new FormControl(null, Validators.required),
+                currency: new FormControl(null, Validators.required)
+            });
+        } else {
+            this.router.navigateByUrl('/');
+        }
+    }
+
+    getDistance() {
+        this.adService.getDistanceTo(this.ad.location,
+            error => this.distance = null,
+            calculatedDistance => this.distance = Math.trunc(calculatedDistance)
+        );
+    }
+
+    onSendOffert() {
+        this.authService.getUserId()
+            .then((userId) => {
+                let offert = new OffertModel(
+                    this.ad.id,
+                    String(userId),
+                    undefined,
+                    this.offertForm.value.solution,
+                    this.offertForm.value.price,
+                    this.offertForm.value.currency,
+                    undefined
+                );
+                //TODO: implement it
+                this.offertService.addOffert(offert)
+                    .subscribe(
+                        data => { console.log(data) },
+                        error => { console.log(error) }
+                    );
+            })
+            .catch((error) => console.log(error));
+    }
 }
