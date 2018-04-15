f1e4368aab93adf564959825262cf389eadbde67
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-04-15 12:57:56 +0300

    UserService: add method for enable notification and update user selected categories

--- assets/app/auth/user.service.ts
+++ assets/app/auth/user.service.ts
@@ -44,6 +44,51 @@ export class UserService {
             });
     }
 
+    getUserSelectedCategories() {
+        const token = localStorage.getItem('token')
+            ? '?token=' + localStorage.getItem('token')
+            : '';
+        return this.http.get(SERVER_PATH + '/my-account/setari/user-info/categories' + token)
+            .map((response: Response) => response.json())
+            .catch((error: Response) => {
+                this.errorService.handleError(error);
+                return Observable.throw(error)
+            });
+    }
+
+    setUserCategory(categoryName, isSelected) {
+        const token = localStorage.getItem('token')
+            ? '?token=' + localStorage.getItem('token')
+            : '';
+
+        let body = JSON.stringify({ categoryName: categoryName, isSelected: isSelected });
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return this.http.patch(SERVER_PATH + '/my-account/setari/user-info/categories' + token, body, { headers: headers })
+            .map((response: Response) => response.json())
+            .catch((error: Response) => {
+                this.errorService.handleError(error);
+                return Observable.throw(error)
+            });
+    }
+
+    updateCertificates(userId) {
+        //TODO
+    }
+
+    enableUserNotification(isEnabled) {
+        const token = localStorage.getItem('token')
+            ? '?token=' + localStorage.getItem('token')
+            : '';
+        let body = JSON.stringify({ isEnabled: isEnabled });
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return this.http.patch(SERVER_PATH + '/my-account/setari/user-info/notification' + token, body, { headers: headers })
+            .map((response: Response) => response.json())
+            .catch((error: Response) => {
+                this.errorService.handleError(error);
+                return Observable.throw(error)
+            });
+    }
+
     updateUserSettingsInfo(user: User) {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
