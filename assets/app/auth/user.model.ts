2815d6f8227492973965dd6087954c52df4c3464
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-04-15 12:58:31 +0300

    User: add field for notification range

--- assets/app/auth/user.model.ts
+++ assets/app/auth/user.model.ts
@@ -1,10 +1,11 @@
 export class User {
     constructor(public email: string,
-                public password: string,
-                public name?: string,
-                public phone?: string,
-                public location?: string,
-                public experienceYears?: number,
-                public biography?: string) {
+        public password: string,
+        public name?: string,
+        public phone?: string,
+        public location?: string,
+        public experienceYears?: number,
+        public biography?: string,
+        public notificationRange?: number) {
     }
 }
