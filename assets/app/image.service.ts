33ca914487a3298f266d5cc6ad6a3cdcdeffe4c3
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-05-08 00:01:36 +0300

    ImageService: implement upload certificate

--- assets/app/image.service.ts
+++ assets/app/image.service.ts
@@ -11,30 +11,20 @@ export class ImageService {
 
     constructor(private http: Http) { }
 
-    uploadCertificate(file) {
+    uploadCertificate(file, title, categoryId) {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
 
         let formData = new FormData();
         formData.append('certificateImage', file, file.name);
+        formData.append('title', title);
+        formData.append('category', categoryId);
 
         let headers = new Headers({ 'enctype': 'multipart/form-data' });
         let options = new RequestOptions({ headers: headers });
 
         return this.http.post(SERVER_PATH + '/my-account/setari/user-info/certificate' + token, formData, options)
-            .map((response: Response) => {
-                return response.json();
-            })
-            .catch((error: Response) => { return Observable.throw(error) });
-    }
-
-    deleteCertificate(fileName) {
-        const token = localStorage.getItem('token')
-            ? '?token=' + localStorage.getItem('token')
-            : '';
-
-        return this.http.delete(SERVER_PATH + '/my-account/setari/user-info/certificate/' + fileName + token, )
             .map((response: Response) => {
                 return response.json();
             })
