a68ec802dce00c4567187a9a3135dcdbf7924eb0
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-05-19 10:21:02 +0300

    AdAdmin: get ad main image

--- assets/app/administration/my-ads/ad-admin.component.ts
+++ assets/app/administration/my-ads/ad-admin.component.ts
@@ -1,21 +1,32 @@
 import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 import { AdModel } from "../../ad/ad.model";
 import { AdService } from "../../ad/ad.service";
+import { ImageService } from "../../image.service";
 
 @Component({
     selector: 'my-ad-admin',
-    styles: [``],
+    styles: [`
+        #adImage {
+            max-height: 120px;
+        }
+    `],
     templateUrl: './ad-admin.component.html'
 })
 export class AdAdminComponent implements OnInit {
 
     @Input() ad: AdModel;
+    adImage: any;
+
     @Output() removeEvent = new EventEmitter<AdModel>();
 
-    constructor(private adService: AdService) { }
+    constructor(private adService: AdService, private imageService: ImageService) { }
 
     ngOnInit() {
         this.getRemainingTime();
+        this.adService.getAdMainImage(this.ad.id, this.ad.categoryName)
+            .subscribe(
+                data => this.adImage = this.imageService.getBase64Image(data._body)
+            );
     }
 
     getRemainingTime() {
